33cc2eeaf59d969a64d9cc142f3f3d15e7a23c66
Nicholas Petree <123441251+Pete1230z@users.noreply.github.com>
2023-06-15 22:16:16 -0700

    Grid will appear NxN

--- JS.js
+++ JS.js
@@ -4,11 +4,9 @@ form.addEventListener("submit", function(e) {
 	e.preventDefault();
 
 var input = form.querySelector("input");
-var boxInput = input.value;
+var boxInput = input.value * input.value;
 gridContainer = document.getElementById("gridContainer");
 console.log(boxInput);
-
-gridContainer.innerHTML = "";
 
 for (var i = 0; i < boxInput; i++) {
 	var div = document.createElement("div");
