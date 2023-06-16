37b323fb00fb1ceb440c57f05857273f840d6258
Nicholas Petree <123441251+Pete1230z@users.noreply.github.com>
2023-06-16 13:53:47 -0700

    Made grids fit NxN, in fact too many are created

--- JS.js
+++ JS.js
@@ -4,11 +4,13 @@ form.addEventListener("submit", function(e) {
 	e.preventDefault();
 
 var input = form.querySelector("input");
-var boxInput = input.value * input.value;
+var boxInput = input.value 
 gridContainer = document.getElementById("gridContainer");
 console.log(boxInput);
 
-for (var i = 0; i < boxInput; i++) {
+for (var i = 0; i < boxInput * boxInput; i++) {
+	gridContainer.style.gridTemplateColumns =  `repeat(${boxInput}, 1fr)`;
+	gridContainer.style.gridTemplateRows =  `repeat(${boxInput}, 1fr)`;
 	var div = document.createElement("div");
 	console.log(div);
 	div.className = "grid";
